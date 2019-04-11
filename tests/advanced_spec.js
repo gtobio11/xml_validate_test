5d7a93b2dff26676602ea5b825d037f65cb15aa3
gtobio11 <gtobio11@gmail.com>
2019-04-11 11:56:46 +0900

    Revert "[]"
    This reverts commit 19b007b395d1887de4e75cec1bf17d352d5b056a.

--- tests/advanced_spec.js
+++ tests/advanced_spec.js
@@ -3,9 +3,7 @@
 const { isValidXML } = require("../src");
 
 describe("advanced validator test", () => {
-  describe("given valid xml", () => {
-    it("should return true for an xml with a self closing node without blank", () => {
-      expect(isValidXML("<a/>")).toBeTruthy();
-    });
+  it("FILL ME", () => {
+    // FILL ME
   });
 });
